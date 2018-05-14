f8748ca7b64da22fd7374de00f0b07b7b4416dee
Nataniel Ben-Ami <Nate0281982@gmail.com>
2018-05-14 20:48:38 +0300

    babel added

--- js/app-compiled.js
+++ js/app-compiled.js
@@ -1,5 +1,7 @@
+'use strict';
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function Enemy() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,14 +12,14 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -31,10 +33,9 @@ Enemy.prototype.render = function() {
 // Place the player object in a variable called player
 
 
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -44,4 +45,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
